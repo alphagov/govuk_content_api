8b2252496e70823ab7c3456b21e25097547d686f
James Stewart <jys@ketlai.co.uk>
2012-06-02 22:07:23 +0100

    Remove some bundler-specific (copy and pasted) code

--- govuk_content_api.rb
+++ govuk_content_api.rb
@@ -31,9 +31,6 @@ end
 def locate_gem(name)
   spec = Bundler.load.specs.find{|s| s.name == name }
   raise GemNotFound, "Could not find gem '#{name}' in the current bundle." unless spec
-  if spec.name == 'bundler'
-    return File.expand_path('../../../', __FILE__)
-  end
   spec.full_gem_path
 end
 
