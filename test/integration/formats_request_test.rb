539f49a4dadb8719961d6c65378d89ea3d3a1e7d
Mazz Mosley <mazz.mosley@digital.cabinet-office.gov.uk>
2012-08-22 16:37:46 +0100

    Business edition test
    Also some re-factoring now the duplication is clear.

--- test/integration/formats_request_test.rb
+++ test/integration/formats_request_test.rb
@@ -8,22 +8,57 @@ class FormatsRequestTest < GovUkContentApiTest
 		@tag2 = FactoryGirl.create(:tag, tag_id: 'crime/batman')
 	end
 
+	def _assert_base_response_info(parsed_response)
+		assert_equal 'ok', parsed_response["response"]["status"]
+		assert parsed_response["response"]["result"].has_key?('title')
+		assert parsed_response["response"]["result"].has_key?('id')
+	end
+
+	def _assert_has_expected_fields(parsed_response, fields)
+		fields.each do |field|
+			assert parsed_response.has_key?(field), 'Field #{field} is MISSING'
+		end
+	end
+
 	def test_answer_edition
-		artefact = FactoryGirl.create(:artefact, slug: 'batman', owning_app: 'publisher')
-		artefact.sections = [@tag1.tag_id]
-		artefact.save!
+		artefact = FactoryGirl.create(:artefact, slug: 'batman', owning_app: 'publisher', sections: [@tag1.tag_id])
 		answer = FactoryGirl.create(:edition, slug: artefact.slug, body: 'Important batman information', panopticon_id: artefact.id, state: 'published')
 
 		get '/batman.json'
 		parsed_response = JSON.parse(last_response.body)
 
 		assert last_response.ok?
+		_assert_base_response_info(parsed_response)
+
 		fields = parsed_response["response"]["result"]["fields"]
 
-		assert fields.has_key?('tag_ids')
-		assert fields.has_key?('alternative_title')
-		
-		assert_equal 'ok', parsed_response["response"]["status"]
-		assert_equal "<p>Important batman information</p>\n", fields["body"]
+		expected_fields = ['tag_ids', 'alternative_title', 'overview', 'body', 'section']
+
+		_assert_has_expected_fields(fields, expected_fields)		
+		assert_equal "Important batman information", fields["body"]
+	end
+
+	def test_business_support_edition
+		artefact = FactoryGirl.create(:artefact, slug: 'batman', owning_app: 'publisher', sections: [@tag1.tag_id])
+		business_support = FactoryGirl.create(:business_support_edition, slug: artefact.slug, 
+																short_description: "No policeman's going to give the Batmobile a ticket", min_value: 100, 
+																max_value: 1000, panopticon_id: artefact.id, state: 'published')
+		business_support.parts[0].body = "Lalalala"
+		business_support.save!
+
+		get '/batman.json'
+		parsed_response = JSON.parse(last_response.body)
+
+		assert last_response.ok?
+		_assert_base_response_info(parsed_response)
+
+		fields = parsed_response["response"]["result"]["fields"]
+		expected_fields = ['tag_ids', 'alternative_title', 'overview', 'section', 
+												'short_description', 'min_value', 'max_value', 'parts']
+
+		_assert_has_expected_fields(fields, expected_fields)
+		assert_false fields.has_key?('body')
+		assert_equal "Lalalala", fields['parts'][0]["body"]
+
 	end
 end
