b51190102e8063c4d363a341e87659942f37a011
James Stewart <jys@ketlai.co.uk>
2012-08-29 14:42:40 +0100

    Configure simplecov
    This gives us information about test coverage. Making it work properly involved moving some
    pieces of test setup around.
    
    Simplecov will only be invoked if the USE_SIMPLECOV environment variable is set so it
    won't normally run on local machines.

--- Rakefile
+++ Rakefile
@@ -6,7 +6,7 @@ require "rake/testtask"
 
 Rake::TestTask.new do |t|
   t.libs << "test"
-  t.libs << File.dirname(__FILE__)
+  # t.libs << File.dirname(__FILE__)
   t.test_files = FileList["test/**/*_test.rb"]
   t.verbose = true
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,19 +1,23 @@
-ENV['RACK_ENV'] = 'test'
-
-require "bundler"
-Bundler.require(:default, ENV['RACK_ENV'])
-
-require 'simplecov'
-SimpleCov.start do
-  add_filter "/test/"
+if ENV["USE_SIMPLECOV"]
+  require "simplecov"
+  require "simplecov-rcov"
+  SimpleCov.start 'test_frameworks'
+  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 end
 
-require_relative '../govuk_content_api'
+ENV['RACK_ENV'] = 'test'
+
+$LOAD_PATH << File.expand_path('../../', __FILE__)
+$LOAD_PATH << File.expand_path('../../lib', __FILE__)
+
 require 'test/unit'
 require 'rack/test'
+
 require 'database_cleaner'
 require 'mocha'
+require 'shoulda'
 require 'factory_girl'
+require 'govuk_content_api'
 require 'govuk_content_models/test_helpers/factories'
 
 DatabaseCleaner.strategy = :truncation
@@ -34,5 +38,4 @@ class GovUkContentApiTest < Test::Unit::TestCase
   def teardown
     DatabaseCleaner.clean
   end
-
 end
