c7341d99b519f08316e1a1f18c05e1de4c68c110
James Stewart <jys@ketlai.co.uk>
2012-10-07 10:27:42 +0100

    Add ExceptionMailer
    We're not currently reporting content API exceptions. This sets them up in the same
    way as rummager. We may shortly swap that out to use some centralised exception
    handling but for now we should know about them.

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 require_relative 'env'
+require_relative 'lib/exception_mailer'
 
 def config_for(kind)
   if ! File.exists?(File.expand_path("../#{kind}.yml", __FILE__))
@@ -15,5 +16,14 @@ configure do
   mongoid_config_file = File.expand_path("mongoid.yml", File.dirname(__FILE__))
   if File.exists?(mongoid_config_file)
     ::Mongoid.load!(mongoid_config_file)
+  end
+end
+
+configure :production do
+  if File.exist?("aws_secrets.yml")
+    disable :show_exceptions
+    use ExceptionMailer, YAML.load_file("aws_secrets.yml"),
+        to: ['govuk-exceptions@digital.cabinet-office.gov.uk'],
+        from: '"Winston Smith-Churchill" <winston@alphagov.co.uk>'
   end
 end
