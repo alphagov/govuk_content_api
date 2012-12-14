734c62946ac02e1892d0f010b0ee0185e65d4341
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-12-14 15:23:27 +0000

    Use the fake pagination code for licences.

--- test/requests/licence_format_test.rb
+++ test/requests/licence_format_test.rb
@@ -13,16 +13,24 @@ class LicenceFormatsTest < GovUkContentApiTest
   it "should return an empty list if not given any IDs" do
     get "/licences.json"
     assert last_response.ok?
-    assert JSON.parse(last_response.body)['results'].empty?,
+    parsed_response = JSON.parse(last_response.body)
+    assert parsed_response['results'].empty?,
       "List of results not found or not empty"
+
+    assert_has_values parsed_response, "total" => 0, "current_page" => 1,
+                                       "pages" => 1
   end
 
   it "should return an empty list if none of the IDs matched" do
     create_stub_licence
     get "/licences.json?ids=abc"
     assert last_response.ok?
-    assert JSON.parse(last_response.body)['results'].empty?,
+    parsed_response = JSON.parse(last_response.body)
+    assert parsed_response['results'].empty?,
       "List of results not found or not empty"
+
+    assert_has_values parsed_response, "total" => 0, "current_page" => 1,
+                                       "pages" => 1
   end
 
   it "should return a list of matching artefacts and licence details" do
@@ -34,6 +42,9 @@ class LicenceFormatsTest < GovUkContentApiTest
     assert_equal 1, parsed_response['results'].count
     assert_equal stub_licence.licence_identifier,
       parsed_response['results'].first['details']['licence_identifier']
+
+    assert_has_values parsed_response, "total" => 1, "current_page" => 1,
+                                       "pages" => 1
   end
 
   it "should not clobber artefact requests that start with 'licences'" do
