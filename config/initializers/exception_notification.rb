e6f0b389bc4db6d03607bc10c6a881fa6881b045
James Stewart <jys@ketlai.co.uk>
2013-01-30 15:54:17 +0000

    Make aws secret paths consistent

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,7 +1,8 @@
 configure :production do
-  if File.exist?("../aws_secrets.yml")
+  aws_secrets = File.expand_path("../aws_secrets.yml", File.dirname(__FILE__))
+  if File.exist?(aws_secrets)
     disable :show_exceptions
-    use ExceptionMailer, YAML.load_file("aws_secrets.yml"),
+    use ExceptionMailer, YAML.load_file(aws_secrets),
         to: ['govuk-exceptions@digital.cabinet-office.gov.uk'],
         from: '"Winston Smith-Churchill" <winston@alphagov.co.uk>',
         subject: '[Content API exception]'
