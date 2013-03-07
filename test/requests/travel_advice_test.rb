e8a4120400895be51bc7ea25a4851a90bfed2161
Jack Franklin <jack@jackfranklin.net>
2013-03-07 14:38:53 +0000

    Reorganise the tests to be clearer

--- test/requests/travel_advice_test.rb
+++ test/requests/travel_advice_test.rb
@@ -87,6 +87,54 @@ class TravelAdviceTest < GovUkContentApiTest
 
   describe "loading data for a travel advice country page" do
 
+    it "should return details for a country with published advice" do
+      artefact = FactoryGirl.create(:artefact, slug: 'foreign-travel-advice/aruba', state: 'live',
+                                    kind: 'travel-advice', owning_app: 'travel-advice-publisher', name: "Aruba travel advice",
+                                    description: "This is the travel advice for people planning a visit to Aruba.")
+      edition = FactoryGirl.build(:travel_advice_edition, country_slug: 'aruba',
+                                  title: "Travel advice for Aruba", overview: "This is the travel advice for people planning a visit to Aruba.",
+                                  change_description: "Some stuff changed",
+                                  summary: "This is the summary\n------\n",
+                                  alert_status: ["avoid_all_but_essential_travel_to_parts","avoid_all_travel_to_parts"])
+      edition.parts.build(title: "Part One", slug: 'part-one', body: "This is part one\n------\n")
+      edition.parts.build(title: "Part Two", slug: 'part-two', body: "And some more stuff in part 2.")
+      edition.save!
+      Timecop.travel(2.days.ago) do
+        edition.publish!
+      end
+
+      get '/foreign-travel-advice%2Faruba.json'
+      assert last_response.ok?
+
+      parsed_response = JSON.parse(last_response.body)
+
+      assert_base_artefact_fields(parsed_response)
+      assert_equal 'travel-advice', parsed_response["format"]
+      assert_equal 'Travel advice for Aruba', parsed_response["title"]
+      assert_equal edition.published_at.xmlschema, parsed_response["updated_at"]
+
+      details = parsed_response["details"]
+      assert_equal 'This is the travel advice for people planning a visit to Aruba.', details['description']
+      assert_equal '<p>Some stuff changed</p>', details['change_description'].strip
+      assert_equal '<h2>This is the summary</h2>', details['summary'].strip
+      assert_equal ["avoid_all_but_essential_travel_to_parts","avoid_all_travel_to_parts"], details['alert_status']
+
+      # Country details
+      assert_equal({"name" => "Aruba", "slug" => "aruba"}, details["country"])
+
+      # Parts
+      parts = details["parts"]
+      assert_equal 2, parts.length
+
+      assert_equal "Part One", parts[0]["title"]
+      assert_equal "part-one", parts[0]["slug"]
+      assert_equal "<h2>This is part one</h2>", parts[0]["body"].strip
+
+      assert_equal "Part Two", parts[1]["title"]
+      assert_equal "part-two", parts[1]["slug"]
+      assert_equal "<p>And some more stuff in part 2.</p>", parts[1]["body"].strip
+    end
+
     describe "loading related links data" do
 
       it "should include related links from the foreign-travel-advice index page" do
@@ -180,55 +228,6 @@ class TravelAdviceTest < GovUkContentApiTest
         assert_equal ["Pies", "Sausages", "Cake"], related_links.map {|l| l["title"] }
       end
 
-    end
-
-
-    it "should return details for a country with published advice" do
-      artefact = FactoryGirl.create(:artefact, slug: 'foreign-travel-advice/aruba', state: 'live',
-                                    kind: 'travel-advice', owning_app: 'travel-advice-publisher', name: "Aruba travel advice",
-                                    description: "This is the travel advice for people planning a visit to Aruba.")
-      edition = FactoryGirl.build(:travel_advice_edition, country_slug: 'aruba',
-                                  title: "Travel advice for Aruba", overview: "This is the travel advice for people planning a visit to Aruba.",
-                                  change_description: "Some stuff changed",
-                                  summary: "This is the summary\n------\n",
-                                  alert_status: ["avoid_all_but_essential_travel_to_parts","avoid_all_travel_to_parts"])
-      edition.parts.build(title: "Part One", slug: 'part-one', body: "This is part one\n------\n")
-      edition.parts.build(title: "Part Two", slug: 'part-two', body: "And some more stuff in part 2.")
-      edition.save!
-      Timecop.travel(2.days.ago) do
-        edition.publish!
-      end
-
-      get '/foreign-travel-advice%2Faruba.json'
-      assert last_response.ok?
-
-      parsed_response = JSON.parse(last_response.body)
-
-      assert_base_artefact_fields(parsed_response)
-      assert_equal 'travel-advice', parsed_response["format"]
-      assert_equal 'Travel advice for Aruba', parsed_response["title"]
-      assert_equal edition.published_at.xmlschema, parsed_response["updated_at"]
-
-      details = parsed_response["details"]
-      assert_equal 'This is the travel advice for people planning a visit to Aruba.', details['description']
-      assert_equal '<p>Some stuff changed</p>', details['change_description'].strip
-      assert_equal '<h2>This is the summary</h2>', details['summary'].strip
-      assert_equal ["avoid_all_but_essential_travel_to_parts","avoid_all_travel_to_parts"], details['alert_status']
-
-      # Country details
-      assert_equal({"name" => "Aruba", "slug" => "aruba"}, details["country"])
-
-      # Parts
-      parts = details["parts"]
-      assert_equal 2, parts.length
-
-      assert_equal "Part One", parts[0]["title"]
-      assert_equal "part-one", parts[0]["slug"]
-      assert_equal "<h2>This is part one</h2>", parts[0]["body"].strip
-
-      assert_equal "Part Two", parts[1]["title"]
-      assert_equal "part-two", parts[1]["slug"]
-      assert_equal "<p>And some more stuff in part 2.</p>", parts[1]["body"].strip
     end
 
     describe "loading assets from asset-manager" do
