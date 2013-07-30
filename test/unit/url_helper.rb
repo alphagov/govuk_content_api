0dd22d3daea9747c05ea5f1d1923485ea4134e07
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-07-30 15:53:32 +0000

    Test the new URL helper class.

---
+++ test/unit/url_helper.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+require "url_helper"
+
+describe URLHelper do
+  it "should use the app's `url` method when there is no prefix" do
+    mock_app = mock("app") do
+      expects(:url).with("/foobang").returns("http://example.com/foobang")
+    end
+    helper = URLHelper.new(mock_app, nil, nil)
+    assert_equal "http://example.com/foobang", helper.api_url("/foobang")
+  end
+
+  it "should use the app's `url` method when the prefix is empty" do
+    mock_app = mock("app") do
+      expects(:url).with("/foobang").returns("http://example.com/foobang")
+    end
+    helper = URLHelper.new(mock_app, nil, "")
+    assert_equal "http://example.com/foobang", helper.api_url("/foobang")
+  end
+
+  it "should use the website root when there is an API prefix" do
+    mock_app = mock("app") do
+      expects(:uri).never
+    end
+    helper = URLHelper.new(mock_app, "http://example.com", "api")
+    assert_equal "http://example.com/api/foobang", helper.api_url("/foobang")
+  end
+
+  it "should produce public web URLs" do
+    mock_app = mock("app") do
+      expects(:uri).never
+    end
+    helper = URLHelper.new(mock_app, "http://example.com", nil)
+    assert_equal "http://example.com/foobang", helper.public_web_url("/foobang")
+  end
+end
