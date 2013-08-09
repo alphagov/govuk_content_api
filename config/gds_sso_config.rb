2e5ca9ad9829b44df4eef4f2d87c8b0165824a9c
James Smith <james@floppy.org.uk>
2013-08-09 15:24:10 +0100

    Load Oauth details from ENV

--- config/gds_sso_config.rb
+++ config/gds_sso_config.rb
@@ -1,6 +1,6 @@
 ::GDS::SSO.config do |config|
   config.user_model     = "ReadOnlyUser"
-  config.oauth_id       = 'abcdefghjasndjkasndcontentapi'
-  config.oauth_secret   = 'secret'
+  config.oauth_id       = ENV['CONTENT_API_OAUTH_ID']
+  config.oauth_secret   = ENV['CONTENT_API_OAUTH_SECRET']
   config.oauth_root_url = Plek.current.find("signon")
 end
