85e4f7d6d13a3b44c26e0cddc0321cff88ac5995
James Smith <james@floppy.org.uk>
2013-08-09 15:26:29 +0100

    Load asset-manager bearer token from environment
    And provide instructions for generating it.

--- config/gds_sso_config.rb
+++ config/gds_sso_config.rb
@@ -1,6 +1,6 @@
 ::GDS::SSO.config do |config|
   config.user_model     = "ReadOnlyUser"
-  config.oauth_id       = ENV['CONTENT_API_OAUTH_ID']
-  config.oauth_secret   = ENV['CONTENT_API_OAUTH_SECRET']
+  config.oauth_id       = ENV['CONTENTAPI_OAUTH_ID']
+  config.oauth_secret   = ENV['CONTENTAPI_OAUTH_SECRET']
   config.oauth_root_url = Plek.current.find("signon")
 end
