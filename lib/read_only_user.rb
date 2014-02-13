ce5337269cdf3655549e192a19c87b094427d725
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2014-02-13 19:27:25 +0000

    Return an empty array for consistent behaviour with AR:Base#where

--- lib/read_only_user.rb
+++ lib/read_only_user.rb
@@ -12,7 +12,7 @@ class ReadOnlyUser < OpenStruct
 
   # For compatibility with https://github.com/alphagov/gds-sso/commit/9f2ae189117eca1758dea108923d15c6fe2b7de7
   def self.where(uid)
-    nil
+    []
   end
 
   def self.create!(auth_hash, options={})
