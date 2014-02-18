e383a9b8d9a257ce0a10600656a4764d6494685f
Jordan Hatch <jordan@jordanh.net>
2014-02-18 13:55:42 +0000

    Allow tagged artefacts to be grouped by format type
    Add a new presenter, GroupedResultSetPresenter, which will group the
    items in the provided result set by their kind/format.
    
    Requests to /with_tag.json?group_by=format will receive a different
    response built by this presenter, instead of the regular
    ResultSetPresenter.
    
    For now, the list of groupings is hard-coded in the new presenter, along
    with the labels which should be returned for each group.

---
+++ lib/presenters/grouped_result_set_presenter.rb
@@ -0,0 +1,51 @@
+class GroupedResultSetPresenter < ResultSetPresenter
+  def present
+    paginated_response_base.merge(
+      "grouped_results" => grouped_results.map do |(group,formats), results|
+        {
+          "name" => group,
+          "formats" => formats,
+          "items" => results.map {|result|
+            @result_presenter_class.new(result, @url_helper).present
+          }
+        }
+      end
+    )
+  end
+
+  private
+  def grouped_results
+    @result_set.results.group_by {|a|
+      display_groups.detect {|group, formats| formats.include?(a.kind) }
+    }
+  end
+
+  def display_groups
+    {
+      "Services" => ["answer", "guide", "licence", "transaction"],
+      "Statutory guidance" => ["statutory_guidance"],
+      "Guidance" => ["guidance", "detailed_guide"],
+      "Document collections" => ["document_collection"],
+      "Forms" => ["form"],
+      "Maps" => ["map"],
+      "Statistics" => ["statistics", "statistical_data_set"],
+      "Research and analysis" => ["research"],
+      "Independent reports" => ["independent_report"],
+      "Impact assessments" => ["impact_assessment"],
+      "Policy papers" => ["policy_paper"],
+      "Consultations" => ["consultation"],
+      "Announcements" => [
+        "transcript",
+        "draft_text",
+        "speaking_notes",
+        "written_statement",
+        "oral_statement",
+        "authored_article",
+        "news_story",
+        "press_release",
+        "government_response",
+        "world_location_news_article"
+      ]
+    }
+  end
+end
