f0164109b9c85b9ce46969e587c68df72758b1ea
Stephen Best <bestie@gmail.com>
2014-06-26 09:21:14 +0100

    Generalise content storage and presentation for specialist docs
    All document specific fields will be stored in a Hash field such that all
    document types can be handled by a single mongoid class and presenter.
    
    Adding more specialist document types should not require changes to the
    Content API.

--- test/requests/specialist_document_test.rb
+++ test/requests/specialist_document_test.rb
@@ -14,17 +14,11 @@ class SpecialistDocumentTest < GovUkContentApiTest
         title: "Private Healthcare Investigation",
         summary: "This is the summary",
         body: "<p>This is the body</p>",
-        opened_date: Date.parse("2013-03-21"),
-        closed_date: nil,
-        case_type: "market-investigation",
-        case_type_label: "Market investigation",
-        case_state: "open",
-        case_state_label: "Open",
-        market_sector: "healthcare",
-        market_sector_label: "Healthcare",
-        outcome_type: "referred",
-        outcome_type_label: "Referred",
-        headers: [],
+        details: {
+          "opened_date" => "2013-03-21",
+          "case_type" => "market-investigation",
+          "case_type_label" => "Market investigation",
+        }
       }
 
       document_attributes = document_defaults.merge(document_attributes)
@@ -55,11 +49,7 @@ class SpecialistDocumentTest < GovUkContentApiTest
       assert_equal 'Private Healthcare Investigation', parsed_response["title"]
       assert_equal 'This is the summary', parsed_response["details"]["summary"]
       assert_equal '2013-03-21', parsed_response["details"]["opened_date"]
-      assert_equal nil, parsed_response["details"]["closed_date"]
       assert_equal "market-investigation", parsed_response["details"]["case_type"]
-      assert_equal "open", parsed_response["details"]["case_state"]
-      assert_equal "healthcare", parsed_response["details"]["market_sector"]
-      assert_equal "referred", parsed_response["details"]["outcome_type"]
     end
 
     it "should include facet labels in the json" do
@@ -67,9 +57,6 @@ class SpecialistDocumentTest < GovUkContentApiTest
       get '/mhra-drug-alerts/private-healthcare-investigation.json'
 
       assert_equal "Market investigation", parsed_response["details"]["case_type_label"]
-      assert_equal "Open", parsed_response["details"]["case_state_label"]
-      assert_equal "Healthcare", parsed_response["details"]["market_sector_label"]
-      assert_equal "Referred", parsed_response["details"]["outcome_type_label"]
     end
 
     it "should include the body of the rendered document" do
@@ -80,36 +67,6 @@ class SpecialistDocumentTest < GovUkContentApiTest
       get "/#{@artefact.slug}.json"
 
       assert_equal html_body, parsed_response['details']['body']
-    end
-
-    it "should provide hierarchical headers for the document body" do
-      example_headers = [
-        {
-          "text" => "Heading",
-          "id" => "heading",
-          "level" => 2,
-          "headers" => [
-            {
-              "text" => "Subheading",
-              "id" => "subheading",
-              "level" => 3,
-              "headers" => []
-            }
-          ]
-        }
-      ]
-
-      build_rendered_specialist_document!(headers: example_headers)
-
-      get "#{@artefact.slug}.json"
-
-      if last_response.ok?
-        actual_headers = JSON.parse(last_response.body).fetch("details").fetch("headers")
-
-        assert_equal example_headers, actual_headers
-      else
-        fail "RESPONSE: #{last_response.status}" + last_response.body
-      end
     end
   end
 
