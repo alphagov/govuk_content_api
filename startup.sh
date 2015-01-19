4cb20716bd44c88c1bf72f6eaf926387474a3146
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-01-19 12:14:25 +0000

    Remove reference to Rabl
    Rabl is a templating system we used for generating JSON. We stopped using it a
    while ago in c46a5d4759ef089538c89d2ec48fe5df237d1410

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bundle install
-bundle exec mr-sparkle --force-polling --pattern "rabl|rb|ru|txt|yml" -- -p 3022
+bundle exec mr-sparkle --force-polling --pattern "rb|ru|txt|yml" -- -p 3022
