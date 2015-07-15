6d633eacbcee5d775f64994f45529e3e5382f089
Paul Bowsher <paul.bowsher@gmail.com>
2015-07-15 13:58:29 +0100

    Remove tests for old content types
    These content types were removed in
    https://github.com/alphagov/govuk_content_models/pull/302, the tests
    don’t pass with the new version of govuk_content_models.

--- test/requests/specialist_document_test.rb
+++ test/requests/specialist_document_test.rb
@@ -126,55 +126,6 @@ class SpecialistDocumentTest < GovUkContentApiTest
     end
   end
 
-  describe 'loading a published manual section' do
-    def build_rendered_specialist_document!(document_attributes = {})
-
-      document_defaults = {
-        slug: 'guidance/immigration-rules/family-members',
-        title: 'Immigration rules part 8: family members',
-        summary: 'This is the summary',
-        body: '<p>This is the body</p>',
-      }
-
-      document_attributes = document_defaults.merge(document_attributes)
-
-      @artefact = FactoryGirl.create(:artefact,
-        slug: 'guidance/immigration-rules/family-members',
-        state: 'live',
-        kind: 'manual-section',
-        owning_app: 'specialist-publisher',
-        name: 'Immigration rules part 8: family members'
-      )
-
-      @document = FactoryGirl.create(:rendered_specialist_document, document_attributes)
-    end
-
-    it 'should return a successful response' do
-      build_rendered_specialist_document!
-      get "/#{@artefact.slug}.json"
-
-      assert last_response.ok?
-    end
-
-    it 'should return json containing the rendered document' do
-      build_rendered_specialist_document!
-      get "/#{@artefact.slug}.json"
-
-      assert_base_artefact_fields(parsed_response)
-      assert_equal 'manual-section', parsed_response["format"]
-      assert_equal 'Immigration rules part 8: family members', parsed_response["title"]
-    end
-
-    it 'should include the body of the rendered document' do
-      html_body = %Q{<h2 id="heading">Heading</h2>\n}
-
-      build_rendered_specialist_document!(body: html_body)
-      get "/#{@artefact.slug}.json"
-
-      assert_equal html_body, parsed_response['details']['body']
-    end
-  end
-
   describe "artefact but no rendered specialist document" do
     before do
       @artefact = FactoryGirl.create(:artefact,
@@ -190,52 +141,5 @@ class SpecialistDocumentTest < GovUkContentApiTest
       get '/mhra-drug-alerts/private-healthcare-investigation.json'
       assert last_response.not_found?
     end
-  end
-
-  describe 'loading manual change history' do
-    def build_change_history!
-      @manual_slug = 'guidance/immigration-rules'
-      @slug = "#{@manual_slug}/updates"
-
-      @updates = [
-        {
-          "published_at" => "2014-01-01T08:45:00",
-          "slug" => "#{@slug}/a-section",
-          "title" => "Manual section",
-          "change_note" => "Changed a thing",
-        },
-      ]
-
-      FactoryGirl.create(:artefact,
-        slug: @slug,
-        state: 'live',
-        kind: 'manual-change-history',
-        owning_app: 'specialist-publisher',
-        rendering_app: 'manuals-frontend',
-        name: 'Immigration rules updates',
-      )
-
-      ManualChangeHistory.create!(
-        slug: @slug,
-        manual_slug: @manual_slug,
-        updates: @updates,
-      )
-    end
-
-    it 'should return a successful response' do
-      build_change_history!
-      get "/#{@slug}.json"
-
-      assert last_response.ok?
-    end
-
-    it 'should return json containing the manual slug and updates' do
-      build_change_history!
-      get "/#{@slug}.json"
-
-      assert_equal 'manual-change-history', parsed_response["format"]
-      assert_equal 'Immigration rules updates', parsed_response["title"]
-    end
-
   end
 end
