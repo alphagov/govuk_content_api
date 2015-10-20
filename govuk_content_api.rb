286b4ad80a7f0a2feb9f6eec29e75103620b4229
Michael Patrick <michael.patrick@digital.cabinet-office.gov.uk>
2015-10-20 10:15:02 +0100

    Remove `areas` facet
    Now that Business Support API uses GSS codes (the `area_gss_codes` facet) to
    query for Business Support Schemes (https://github.com/alphagov/business-support-api/pull/32),
    `areas` is no longer needed.

--- govuk_content_api.rb
+++ govuk_content_api.rb
@@ -367,7 +367,7 @@ class GovUkContentApi < Sinatra::Application
     set_expiry
 
     facets = {}
-    [:areas, :area_gss_codes, :business_sizes, :locations, :purposes, :sectors, :stages, :support_types].each do |key|
+    [:area_gss_codes, :business_sizes, :locations, :purposes, :sectors, :stages, :support_types].each do |key|
       facets[key] = params[key] if params[key].present?
     end
 
