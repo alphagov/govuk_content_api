190a2cfe1b395d973a6fe33571c34ba067c4d64d
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2016-05-27 18:49:11 +0100

    Archive the local authorities API
    From our server logs it looks like nothing is using this - there have been no
    requests for it in the last week. We are going to stop importing local
    authorities into the shared database soon and so this data will no longer be
    updated after that, so we might as well archive it now.
    
    Also stop linking to the API from local authorities embedded in local
    transactions.

--- lib/presenters/local_authority_presenter.rb
+++ lib/presenters/local_authority_presenter.rb
@@ -4,25 +4,13 @@ class LocalAuthorityPresenter
     name snac tier homepage_url
   )
 
-  def initialize(result, url_helper)
+  def initialize(result)
     @result = result
-    @url_helper = url_helper
   end
 
   def present
-    presented = {
-      "id" => local_authority_url(@result),
-    }
-
-    FIELDS.each_with_object(presented) do |field_name, hash|
+    FIELDS.each_with_object({}) do |field_name, hash|
       hash[field_name] = @result.send(field_name)
     end
-
-    presented
-  end
-
-private
-  def local_authority_url(authority)
-    @url_helper.api_url("/local_authorities/#{CGI.escape(authority.snac)}.json")
   end
 end
