1c2d3d2f0484be87be3aeeffea1561f0252412d8
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2016-06-07 11:48:04 +0100

    Refactor local authorities API tests to describe paths
    This looks clearer to me than the previous structure.

--- test/requests/local_authority_request_test.rb
+++ test/requests/local_authority_request_test.rb
@@ -1,25 +1,27 @@
 require "test_helper"
 
 class LocalAuthorityRequestTest < GovUkContentApiTest
-  it "should return 410 if no name or snac code is provided" do
-    get "/local_authorities.json"
-    assert_equal 410, last_response.status
-    assert_status_field "gone", last_response
-  end
+  describe "local authorities index URL" do
+    it "should return 410" do
+      get "/local_authorities.json"
+      assert_equal 410, last_response.status
+      assert_status_field "gone", last_response
+    end
 
-  it "should return 410 if a snac code is provided" do
-    get "/local_authorities/gobble_de_gook.json"
-    assert_equal 410, last_response.status
-    assert_status_field "gone", last_response
-  end
-
-  describe "setting cache-control headers" do
-    it "should set long cache-control headers if no name or snac code is provided" do
+    it "should set long cache-control headers" do
       get "/local_authorities.json"
       assert_equal "public, max-age=#{1.hour.to_i}", last_response.headers["Cache-control"]
     end
+  end
 
-    it "should set long cache-control headers for a snac lookup" do
+  describe "local authorities with SNAC" do
+    it "should return 410" do
+      get "/local_authorities/gobble_de_gook.json"
+      assert_equal 410, last_response.status
+      assert_status_field "gone", last_response
+    end
+
+    it "should set long cache-control headers" do
       get "/local_authorities/00CT.json"
       assert_equal "public, max-age=#{1.hour.to_i}", last_response.headers["Cache-control"]
     end
