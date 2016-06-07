96da9f40a652efad25051e8017a775d2692bc676
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2016-06-07 11:43:29 +0100

    Set cache headers to 1 hour for archived local authorities API
    This was accidentally removed in 190a2cfe1b395d973a6fe33571c34ba067c4d64d.
    Reinstate the tests for the cache headers too.

--- test/requests/local_authority_request_test.rb
+++ test/requests/local_authority_request_test.rb
@@ -12,4 +12,16 @@ class LocalAuthorityRequestTest < GovUkContentApiTest
     assert_equal 410, last_response.status
     assert_status_field "gone", last_response
   end
+
+  describe "setting cache-control headers" do
+    it "should set long cache-control headers if no name or snac code is provided" do
+      get "/local_authorities.json"
+      assert_equal "public, max-age=#{1.hour.to_i}", last_response.headers["Cache-control"]
+    end
+
+    it "should set long cache-control headers for a snac lookup" do
+      get "/local_authorities/00CT.json"
+      assert_equal "public, max-age=#{1.hour.to_i}", last_response.headers["Cache-control"]
+    end
+  end
 end
