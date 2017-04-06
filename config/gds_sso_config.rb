d329cfb62679a518c45c6e96c27b8cc0b209b790
Ruben Arakelyan <ruben@ra.me.uk>
2017-04-06 09:45:05 +0100

    Add fallback OAuth ID and secret
    This commit adds a fallback OAuth ID and secret. This will allow the signon-munging script for development to configure signon for this app locally.

--- config/gds_sso_config.rb
+++ config/gds_sso_config.rb
@@ -1,6 +1,6 @@
-::GDS::SSO.config do |config|
+GDS::SSO.config do |config|
   config.user_model     = "ReadOnlyUser"
-  config.oauth_id       = ENV['OAUTH_ID']
-  config.oauth_secret   = ENV['OAUTH_SECRET']
+  config.oauth_id       = ENV['OAUTH_ID'] || "abcdefg"
+  config.oauth_secret   = ENV['OAUTH_SECRET'] || "secret"
   config.oauth_root_url = Plek.current.find("signon")
 end
